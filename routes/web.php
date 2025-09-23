2782b3d7947edfd3c5f8bacf16ca8e0c570bbb88
Valsym <fanski@yandex.ru>
2025-09-23 18:50:44 +0300

    Созданы таблицы в БД myblog (users, posts, tags, comments и связующие)

--- routes/web.php
+++ routes/web.php
@@ -54,10 +54,10 @@ Route::get("/contact", function () {
     return view("contact");
 })->name('contact');
 
-Route::get("/posts", [PostController::class, "index"]);
+//Route::get("/posts", [PostController::class, "index"]);
 Route::get("/posts/create", [PostController::class, "create"]);
 Route::post("/posts", [PostController::class, "store"]);
-Route::get("/posts/{id}", [PostController::class, "show"]);
+//Route::get("/posts/{id}", [PostController::class, "show"]);
 Route::get("/posts/{id}/edit", [PostController::class, "edit"]);
 Route::put("/posts/{id}", [PostController::class, "update"]);
 Route::delete("/posts/{id}", [PostController::class, "destroy"]);
