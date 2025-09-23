4ea57814e05cb4515b41f9aaeb2ab9337145d2d1
Valsym <fanski@yandex.ru>
2025-09-23 23:04:39 +0300

    Fix error route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
                         @foreach($post->tags as $tag)
                             <span> {{ $tag->name }} </span>
                         @endforeach
-                        <a class="nav-link" href="{{ route('posts', $post->id) }}">далее...</a>
+                        <a class="nav-link" href="{{ route('posts.show',['id' => $post->id]) }}">далее...</a>
                     </div>
                 </div>
             @endforeach
--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ Route::get("/contact", function () {
 Route::get("/posts", [PostController::class, "index"])->name('posts');;
 Route::get("/posts/create", [PostController::class, "create"]);
 Route::post("/posts", [PostController::class, "store"]);
-Route::get("/posts/{id}", [PostController::class, "show"])->name('show');
+Route::get("/posts/{id}", [PostController::class, "show"])->name('posts.show');
 Route::get("/posts/{id}/edit", [PostController::class, "edit"]);
 Route::put("/posts/{id}", [PostController::class, "update"]);
 Route::delete("/posts/{id}", [PostController::class, "destroy"]);
