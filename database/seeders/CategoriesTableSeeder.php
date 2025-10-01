d4192e13e48818eeb661e78f65e61aec51aa6af3
Valsym <fanski@yandex.ru>
2025-10-01 11:41:47 +0300

    Создание функционала редактора постов для блога: 1.2 Наполнение данными (Seeders)

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Создаем категории
+        $categories = ["PHP", "Laravel", "JavaScript", "Yii", "Git",
+            "CSS", "HTML", "ООП", "SOLID", "Docker", "Деплой"];
+        foreach ($categories as $category) {
+            DB::table("categories")->insert([
+                "name" => $category,
+                "slug" => Str::slug($category),
+                "created_at" => now(),
+                "updated_at" => now(),
+            ]);
+        }
+    }
+}
